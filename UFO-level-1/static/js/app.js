c64600643ac262ffcf02925e80074144a1127d7d
Jeannaej <jeannaejy22@gmail.com>
2021-04-21 00:21:30 -0500

    Added code to read the data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+//Read data
+console.log(tableData);
+
+//Use d3 to update each cell's text with per sighting info (date, city, state, country, shape, duration, comment)
+  data.forEach(function(weatherReport) {
+    console.log(weatherReport);
+    var row = tbody.append("tr");
+    Object.entries(weatherReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
