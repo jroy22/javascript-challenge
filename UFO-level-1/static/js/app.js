d72f814769dc61727e4967233b7a660bb6addeac
Jeannaej <jeannaejy22@gmail.com>
2021-04-23 22:17:48 -0500

    added code to only show the filtered sightings on the webpage table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,16 +8,16 @@ var tbody = d3.select("tbody");
 // console.log(tableData);
 
 //Use d3 to update each cell's text with per sighting info (date, city, state, country, shape, duration, comment)
-  data.forEach(function(ufoSightings) {
-    // console.log(ufoSightings);
-    var row = tbody.append("tr");
-    Object.entries(ufoSightings).forEach(function([key, value]) {
-    //console.log(key, value);
-    // Append a cell to the row for each value
-    var cell = row.append("td");
-    cell.text(value);
-    });
-  });
+//   tableData.forEach(function(ufoSightings) {
+//     // console.log(ufoSightings);
+//     var row = tbody.append("tr");
+//     Object.entries(ufoSightings).forEach(function([key, value]) {
+//     //console.log(key, value);
+//     // Append a cell to the row for each value
+//     var cell = row.append("td");
+//     cell.text(value);
+//     });
+//   });
 
 // Getting a reference to the button on the page with the id property set to `filter-btn`
 var button = d3.select("#filter-btn");
@@ -27,32 +27,43 @@ var inputField = d3.select("#datetime");
 
 var form = d3.select("#form");
 
-// This function is triggered when the button is clicked
-function filterButton() {
-    console.log("A button was clicked!");
-    // Use d3 to see the object that dispatched the event
-    console.log(d3.event.target);
-  }
+// // This function is triggered when the button is clicked
+// function filterButton() {
+//     console.log("A button was clicked!");
+//     // Use d3 to see the object that dispatched the event
+//     console.log(d3.event.target);
+//   }
 
 // Use the `on` function in d3 to attach an event to the handler function
-button.on("click", filterButton);
+// button.on("click", filterButton);
+button.on("click", clickEnter);
+form.on("submit", clickEnter);
 
-// Input fields can trigger a change event when new text is entered.
-inputField.on("change", function() {
-    var newDate = d3.event.target.value;
-    console.log(newDate);
-  });
+// // Input fields can trigger a change event when new text is entered.
+// inputField.on("change", function() {
+//     var newDate = d3.event.target.value;
+//     console.log(newDate);
+//   });
 
-  function clickEnter() {
+function clickEnter() {
     // Prevent page form refreshing
     d3.event.preventDefault();
 
+    // Get the value property of the input element
     var searchDate = inputField.property("value");
 
+    //Filtering the data based on the datetime
     var filteredData = tableData.filter(tableData => tableData.datetime === searchDate);
 
     console.log(filteredData);
-  }
 
-  button.on("click", clickEnter);
-  form.on("submit", clickEnter);
+    filteredData.forEach(function(filteredData) {
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(function([key, value]) {
+            //console.log(key, value);
+            // Append a cell to the row for each value
+            var cell = row.append("td");
+            cell.text(value);
+            });
+    });
+}
